423db4b7dc74efc2cea969ce30679bafba9aa7c9
Song Sunho <awslife@Songs-MacBook-Pro.local>
2022-01-25 23:14:15 +0900

    first commit

---
+++ assets/js/plugins/markerwidget.js
@@ -0,0 +1,14 @@
+<script>
+    (function(d, h, m){
+        var js, fjs = d.getElementsByTagName(h)[0];
+        if (d.getElementById(m)){return;}
+        js = d.createElement(h); js.id = m;
+        js.onload = function(){
+            window.makerWidgetComInit({
+            position: "left",          
+            widget: "zemmulcdp5qjnite-q2rzj72kchzymkcm-unq74knlvu2jgnpp"                
+        })};
+        js.src = "https://makerwidget.com/js/embed.js";
+        fjs.parentNode.insertBefore(js, fjs)
+    }(document, "script", "dhm"))
+</script>
